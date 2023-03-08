a88e126ee0bdae155887c5a93aff0f9c1c9b9048
Matias Rolon <matiasrolon1301@gmail.com>
2023-03-08 12:11:04 -0300

    Creacion de los modelos

---
+++ src/main/java/com/ap/ap/models/Usuario.java
@@ -0,0 +1,101 @@
+
+package com.ap.ap.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String titulo;
+    private String descripcion;
+    private String fotoPerfil;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idEdu")
+    private List<Educacion> educacionList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idSkill")
+    private List<Skills> skillList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idExp")
+    private List<Experiencia> experienciaList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idPro")
+    private List<Proyectos> proyectosList;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nombre, String apellido, String titulo, String descripcion, String fotoPerfil) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(String fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+    
+    
+    
+}
