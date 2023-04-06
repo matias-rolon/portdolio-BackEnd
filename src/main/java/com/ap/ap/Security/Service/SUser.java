5fdc4e006281eba7de5f01dd377e469de63d2400
Matias Rolon <matiasrolon1301@gmail.com>
2023-04-06 18:42:00 -0300

    Creacion del servicio de UserLogin

---
+++ src/main/java/com/ap/ap/Security/Service/SUser.java
@@ -0,0 +1,33 @@
+
+package com.ap.ap.Security.Service;
+
+import com.ap.ap.Security.Entity.UserLogin;
+import com.ap.ap.Security.Repository.RUser;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SUser {
+    @Autowired
+    RUser rUser;
+    
+    public Optional<User> getByNombreUsuario(String nombreUsuario){
+        return rUser.findByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existsByNombreUsuario(String nombreUsuario){
+        return rUser.existsByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existsByEmail(String email){
+        return rUser.existsByEmail(email);
+    }
+    
+    public void save(UserLogin usuario){
+        rUser.save(usuario);
+    }
+}
